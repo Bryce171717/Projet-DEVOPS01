307fae87beb5e89702a2b45c067f4731d23ed864
adm0-bjama755@si.cnaf.info <brice.jamain@cnaf.fr>
2024-06-20 18:48:04 +0200

    Envoi V2

--- emr.tf
+++ emr.tf
@@ -1,7 +1,23 @@
 resource "aws_emr_cluster" "spark_cluster" {
   name          = "SparkCluster01"
   release_label = "emr-7.1.0"
-  applications  = ["Hadoop", "Hive", "JupyterEnterpriseGateway", "Livy", "Spark"]
+  applications  = [
+    {
+      Name = "JupyterEnterpriseGateway"
+    },
+    {
+      Name = "Hive"
+    },
+    {
+      Name = "Hadoop"
+    },
+    {
+      Name = "Livy"
+    },
+    {
+      Name = "Spark"
+    }
+  ]
   log_uri       = var.emr_log_uri
 
   ec2_attributes {
@@ -9,25 +25,48 @@ resource "aws_emr_cluster" "spark_cluster" {
     subnet_id                  = var.subnet_id
     emr_managed_master_security_group = var.emr_security_group_master
     emr_managed_slave_security_group  = var.emr_security_group_slave
+    key_name                   = var.emr_key_name
   }
 
+  configurations = <<EOF
+  [
+    {
+      "Classification": "spark-defaults",
+      "Properties": {
+        "spark.jars.packages": "org.mongodb.spark:mongo-spark-connector_2.12:3.0.1",
+        "spark.mongodb.input.uri": "mongodb://${var.mongodb_username}:${var.mongodb_password}@mongodb:27017/database01",
+        "spark.mongodb.output.uri": "mongodb://${var.mongodb_username}:${var.mongodb_password}@mongodb:27017/database01"
+      }
+    }
+  ]
+  EOF
+
   master_instance_group {
-    instance_type = var.instance_type
+    instance_type = "m5.xlarge"
     instance_count = 1
     name = "Master"
+    ebs_config {
+      ebs_block_device {
+        volume_type = "gp2"
+        size        = 32
+        volumes_per_instance = 2
+      }
+    }
   }
 
   core_instance_group {
-    instance_type = var.instance_type
+    instance_type = "m5.xlarge"
     instance_count = 2
     name = "Core"
+    ebs_config {
+      ebs_block_device {
+        volume_type = "gp2"
+        size        = 32
+        volumes_per_instance = 2
+      }
+    }
   }
 
   scale_down_behavior = "TERMINATE_AT_TASK_COMPLETION"
-
-  auto_termination_policy {
-    idle_timeout = 3600
-  }
-
-  service_role = var.emr_service_role
+  service_role        = var.emr_service_role
 }
--- variables.tf
+++ variables.tf
@@ -16,40 +16,46 @@ variable "mongodb_password" {
   sensitive   = true
 }
 
-variable "instance_type" {
-  description = "Type d'instance EC2 pour le cluster Spark et MongoDB"
-  type        = string
-  default     = "m5.xlarge"
-}
-
 variable "subnet_id" {
   description = "ID du sous-réseau où les instances seront déployées"
   type        = string
+  default     = "subnet-00508a1e4ac0faf28"
 }
 
 variable "emr_log_uri" {
   description = "URI du bucket S3 pour les logs EMR"
   type        = string
+  default     = "s3://aws-logs-031131961798-eu-west-3/elasticmapreduce"
 }
 
 variable "emr_instance_profile" {
   description = "Profil d'instance IAM pour les instances EMR"
   type        = string
+  default     = "arn:aws:iam::031131961798:instance-profile/EmrEc2S3Full"
 }
 
 variable "emr_service_role" {
   description = "Rôle de service IAM pour EMR"
   type        = string
+  default     = "arn:aws:iam::031131961798:role/service-role/AmazonEMR-ServiceRole-20240505T140225"
 }
 
 variable "emr_security_group_master" {
   description = "Groupe de sécurité pour le master EMR"
   type        = string
+  default     = "sg-01ceb80944ffa01d6"
 }
 
 variable "emr_security_group_slave" {
   description = "Groupe de sécurité pour les slaves EMR"
   type        = string
+  default     = "sg-0852ccf141b26f385"
+}
+
+variable "emr_key_name" {
+  description = "Nom de la clé SSH pour accéder aux instances EMR"
+  type        = string
+  default     = "SSH_Mongodb"
 }
 
 variable "mongodb_ami" {
@@ -61,6 +67,7 @@ variable "mongodb_ami" {
 variable "mongodb_key_name" {
   description = "Nom de la clé SSH pour accéder à MongoDB"
   type        = string
+  default     = "SSH_Mongodb"
 }
 
 variable "mongodb_security_group" {
